54022bb585c4479d8144709affd4e59928edece1
Adrian Laksana <adrianjlaksana@gmail.com>
2025-04-01 21:45:56 -0700

    moved into server folder and created client folder

--- README.md
+++
@@ -1,2 +0,0 @@
-# NotesApp
-Intro to MERN technology while developing a NotesApp while imitating the needs for a real scenario stakeholder
--- config/allowedOrigins.js
+++
@@ -1,7 +0,0 @@
-const allowedOrigins = [
-    'htpp://localhost:3000',
-    'https://www.yourwebsite.com',
-    'https://yourwebsite.com',
-]
-
-module.exports = allowedOrigins
